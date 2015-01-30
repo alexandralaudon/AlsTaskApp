d2aaf06b3053d2423626338afe60958680465125
Alex <alexandralaudon@gmail.com>
2015-01-29 17:10:48 -0700

    Completed story on users being able to see quotes on the homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    @quotes =
+      [
+        { author: "-Cayla Hayes", text: "gCamp has changed my life! It's the best tool I've ever used."
+        },
+        {author: "-Leta Jaskolski", text: "Before gCamp I was a disorderly slob. Now I'm more organized than I've ever been"
+        },
+        {author: "-Lavern Upton", text: "Don't hesitate - sign up right now! You'll never be the same."
+        }
+      ]
   end
 end

89c83fae67b4412dc874a73d5efc6631264e4d28
Alex <alexandralaudon@gmail.com>
2015-02-13 15:34:55 -0700

    PT Story: Added Users migration and Users routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212223339) do
+ActiveRecord::Schema.define(version: 20150213222744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20150212223339) do
     t.datetime "updated_at"
     t.boolean  "complete"
     t.date     "due_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

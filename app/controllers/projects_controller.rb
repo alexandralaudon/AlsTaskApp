fd0402bc3084c862fe67f8698873c74410cf94f9
Alex <alexandralaudon@gmail.com>
2015-02-19 15:00:30 -0700

    PT Completed 'Users can see a list of projects and add new project' ID:88666304

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,23 @@
 class ProjectsController < ApplicationController
   def index
-    
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path, notice: "Project was successfully created"
+  end
+
+
+private
+
+  def project_params
+    params.require(:project).permit(:name, :created_at, :updated_at)
   end
 
 end

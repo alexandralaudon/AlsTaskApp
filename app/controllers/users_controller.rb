1e15eb1bdca8dc456f7c6a8e2e58b33ccd6bbab1
Alex <alexandralaudon@gmail.com>
2015-02-23 12:54:31 -0700

    PT ID:88925560 completed. Added error message for Users#_userform

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,11 @@ class UsersController <ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to users_path, notice: "User was successfully created!"
+    if @user.save
+      redirect_to users_path, notice: "User was successfully created!"
+    else
+      render :new
+    end
   end
 
   def show
@@ -26,6 +29,8 @@ class UsersController <ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to users_path, notice: "User was successfully updated!"
+    else
+      render :edit
     end
   end
 

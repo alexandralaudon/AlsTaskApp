bfce77a46a0ce5032788777d1dd59cc42d5c5049
Alex <alexandralaudon@gmail.com>
2015-03-03 14:55:00 -0700

    Added IDs for Tasks#_form in order to pass rspec

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -7,6 +7,8 @@ feature 'Tasks' do
     expect(page).to have_no_content('go work hard')
     click_on('New Task')
     expect(current_path).to eq(new_task_path)
+    fill_in :description, with: 'Work hard, play hard'
+    fill_in :due_date, with: "12/12/2015"
 
 
   end

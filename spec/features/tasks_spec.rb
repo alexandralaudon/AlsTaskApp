0398c78c82cfbaa259ef9fbe451dda906ec0e1d9
Alex <alexandralaudon@gmail.com>
2015-03-03 17:02:04 -0700

    Added date format to Tasks#index

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -7,9 +7,10 @@ feature 'Tasks' do
     expect(page).to have_no_content('go work hard')
     click_on('New Task')
     expect(current_path).to eq(new_task_path)
-    fill_in :description, with: 'Work hard, play hard'
-    fill_in :due_date, with: "12/12/2015"
-
+    fill_in "Description", with: 'Work hard, play hard'
+    fill_in "Due date", with: "12/12/2015"
+    click_button('Create Task')
+    
 
   end
 

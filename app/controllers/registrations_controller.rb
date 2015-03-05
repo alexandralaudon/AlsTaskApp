1e9c9d35cd7e4aca658ed7d1cfa8e8c9927a7ac2
Alex <alexandralaudon@gmail.com>
2015-03-04 17:56:40 -0700

    Expanded params requirements for password on Users & Registrations Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < ApplicationController
 
   def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation))    
   end
 end

115423de64de6c758da3b646c3b84ddc3542a4a1
Alex <alexandralaudon@gmail.com>
2015-03-06 14:03:25 -0700

    Added before_action for Tasks Controller for clean up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,16 @@
 class TasksController < ApplicationController
   before_action :require_login
+  before_action :project_instance_variable, except: [:destroy]
 
   def index
-    @project = Project.find(params[:project_id])
     @tasks = @project.tasks
   end
 
   def new
-    @project = Project.find(params[:project_id])
     @task = @project.tasks.new
   end
 
   def create
-    @project = Project.find(params[:project_id])
     @task = @project.tasks.new(task_params)
     if @task.save
       flash[:notice] = "Task was successfully created!"
@@ -23,17 +21,14 @@ class TasksController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:project_id])
     @task = Task.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:project_id])
     @task = Task.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:project_id])
     @task = @project.tasks.find(params[:id])
     if @task.update(task_params)
       redirect_to project_task_path(@project, @task[:id]), notice: "Task was successfully updated!"
@@ -49,6 +44,10 @@ class TasksController < ApplicationController
 
 
   private
+
+  def project_instance_variable
+    @project = Project.find(params[:project_id])
+  end
 
   def task_params
     params.require(:task).permit(:description, :complete, :due_date, :project_id)

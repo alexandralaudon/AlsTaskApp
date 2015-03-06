18b78f1bea54b45552c1f91a8d4c9b4586264dad
Alex <alexandralaudon@gmail.com>
2015-03-05 17:59:07 -0700

    Updated authentication_spec.rb for additional specs on signing in

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -6,18 +6,24 @@ feature 'Authentication' do
     User.create!(first_name:'William', last_name: 'Shakespeare', email: 'tobe@nottobe.net', password:'that is the question', password_confirmation:'that is the question')
   end
 
-  scenario 'User can sign out successfully' do
+  scenario 'User can Sign In successfully' do
     visit root_path
     within(".navbar") {click_link("Sign In")}
     fill_in "Email", with: 'tobe@nottobe.net'
     fill_in "Password", with: 'that is the question'
     click_button("Sign In")
 
-    within(".navbar") {click_link("Sign Out")}
-    expect(current_path).to eq(root_path)
+    within(".alert") {have_content("You have successfully signed in")}
+    within('.jumbotron') { have_content("Your life, organized.")}
   end
 
-  scenario 'Test where you are redirecting to and the flash message' do
+  scenario 'User can see validation messages' do
+      visit sign_in_path
+      click_button("Sign In")
+      within(".well") {have_content("Email / Password combination is invalid")}
+  end
+
+  scenario 'User can sign out successfully' do
     visit root_path
     within(".navbar") {click_link("Sign In")}
     fill_in "Email", with: 'tobe@nottobe.net'

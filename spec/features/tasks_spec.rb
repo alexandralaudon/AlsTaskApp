eee192094b3b9c3dec52d8acd8afd1699a5e5be3
Alex <alexandralaudon@gmail.com>
2015-03-06 13:36:41 -0700

    Restructured tasks_spec to handle user_authentication & nested routes for projects

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -2,25 +2,16 @@ require 'rails_helper'
 
 feature 'Tasks' do
 
-  background do
-    User.create(first_name:'Muhammad', last_name: 'Ali', email: 'bam@pow.com', password: 'ouch', password_confirmation: 'ouch')
-    visit sign_in_path
-    fill_in "Email", with: 'bam@pow.com'
-    fill_in "Password", with: 'ouch'
-    click_button('Sign In')
-  end
-
   scenario 'User creates a task' do
-    visit new_project_path
-    fill_in "Name", with: "Polymorphism"
-    click_button 'Create Project'
-    click_on '0 Tasks'
-    click_link "New Task"
-    
+    user1 = create_user
+    proj1 = create_project
 
-    visit project_tasks_path
-    click_on('New Task')
-    expect(current_path).to eq(new_task_path)
+    visit projects_path
+    click_on("Abstraction")
+    click_on '0 Tasks'
+
+    within(".breadcrumb") {have_content("Projects Abstraction Tasks")}
+    click_link "New Task"
 
     click_button('Create Task')
     expect(page).to have_content('1 error prohibited this form from being saved:')
@@ -29,39 +20,31 @@ feature 'Tasks' do
     fill_in "Due date", with: "12/12/2015"
     click_button('Create Task')
     expect(page).to have_content('Task was successfully created!')
-    expect(page).to have_content('Work hard, play hard')
+    expect(page).to have_content('Projects Abstraction Tasks Work hard, play hard')
   end
 
   scenario 'User edits and deletes a task' do
-    Task.create!(description:'Play hard, Work hard', due_date: '02/02/2015')
+    user1 = create_user
+    proj1 = create_project
+    task1 = create_task(proj1)
 
-    visit tasks_path
-    click_on('Play hard, Work hard')
+    visit projects_path
+    click_link "Abstraction"
+    click_on "1 Task"
+
+    click_on('Simplifying complex reality')
     click_on('Edit Task')
-    fill_in "Description", with: 'Work harddd, play harddd'
+    fill_in "Description", with: 'modeling classes appropriate to the problem'
     fill_in "Due date", with: "12/12/2015"
     click_button('Update Task')
     expect(page).to have_content('Task was successfully updated!')
-    expect(page).to have_content('Work harddd, play harddd')
-    expect(current_path).to eq(task_path(Task.first.id))
-
-    # Ensure the other way to edit page works too
-    visit tasks_path
-    click_on ('Edit')
-    fill_in "Description", with: 'Work wtvr, play wtvr'
-    fill_in "Due date", with: "11/11/2015"
-    click_button('Update Task')
-    expect(page).to have_content('Task was successfully updated!')
-    expect(page).to have_content('Work wtvr, play wtvr')
-    expect(current_path).to eq(task_path(Task.first.id))
-    visit tasks_path
-    expect(page).to have_content('Work wtvr, play wtvr')
-    expect(page).to have_content('11/11/2015')
+    expect(page).to have_content('modeling classes appropriate to the problem')
+    within(".breadcrumb") {click_link "Tasks"}
 
     # Delete task
     click_on('Delete')
     expect(page).to have_content('Task was successfully deleted!')
-    expect(page).to have_no_content('Work wtvr, play wtvr')
+    expect(page).to have_no_content('modeling classes appropriate to the problem')
 
   end
 
---
+++ spec/support/object_creation_methods.rb
@@ -0,0 +1,15 @@
+def create_user
+  User.create(first_name:'Muhammad', last_name: 'Ali', email: 'bam@pow.com', password: 'ouch', password_confirmation: 'ouch')
+  visit sign_in_path
+  fill_in "Email", with: 'bam@pow.com'
+  fill_in "Password", with: 'ouch'
+  click_button('Sign In')
+end
+
+def create_project
+  Project.create!(name: "Abstraction")
+end
+
+def create_task(project)
+  Task.create!(description: "Simplifying complex reality", due_date: '01/01/2000', project_id: project.id)
+end

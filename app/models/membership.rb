89dbdbed6facbf1dcd15a12ebc42080770bf5022
Alex <alexandralaudon@gmail.com>
2015-03-09 17:18:28 -0600

    Added uniqueness scope to Membership Model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   validates_presence_of :user_id, message: "can't be blank"
-  validates_uniqueness_of :user_id, message: 'has already been added to this project'
+  validates_uniqueness_of :user_id, {scope: :project_id, message: 'has already been added to this project'}
 
   ROLE = ['Member', 'Owner']
 

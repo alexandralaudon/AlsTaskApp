30620a8eb851c5870311a98aa3e35970337e3b88
Alex <alexandralaudon@gmail.com>
2015-03-13 15:56:52 -0600

    Replaced User Model method full name with titleize instead of capitalize

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def full_name
-    "#{first_name.capitalize} #{last_name.capitalize}"
+    "#{first_name} #{last_name}".titleize
   end
 
 

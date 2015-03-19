923aecc24103e7a6b5c2646bc5768bd5c9256ec6
Alex <alexandralaudon@gmail.com>
2015-03-18 19:35:51 -0600

    Updated comments_controller_spec to contain before(:each) do with user authentication-syntax error found

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,33 +4,33 @@ describe CommentsController do
   before(:each) do
     user = create_user
     session[:user_id] = user.id
-    project = create_project
-    task = create_task(project)
+    @project = create_project
+    @task = create_task(@project)
   end
 
   describe "POST #create" do
     describe "on success" do
       it "creates a new comment when valid parameters are passed" do
         expect {
-          post :create, task_id: task.id, comment: { message: "I work, or do I...?"}
+          post :create, task_id: @task.id, comment: { message: "I work, or do I...?"}
         }.to change {Comment.all.count}.by(1)
 
         comment = Comment.last
         expect(comment.message).to eq "I work, or do I...?"
         expect(flash[:notice]).to eq nil
-        expect(response).to redirect_to project_task_path(project.id, task.id)
+        expect(response).to redirect_to project_task_path(@project.id, @task.id)
       end
     end
 
     describe "on failure" do
       it "does not add a new comment if it is invalid" do
         expect {
-          post :create, task_id: task.id, comment: { message: ""}
+          post :create, task_id: @task.id, comment: { message: ""}
         }.to_not change {Comment.all.count}
 
         comment = Comment.last
         expect(flash[:notice]).to eq nil
-        expect(response).to redirect_to project_task_path(project.id, task.id)
+        expect(response).to redirect_to project_task_path(@project.id, @task.id)
         expect(assigns(:comment)).to be_a(Comment)
       end
     end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -4,28 +4,34 @@ describe TasksController do
   before(:each) do
     user = create_user
     session[:user_id] = user.id
+    @project = create_project
   end
 
   describe 'GET #index' do
     it "assigns all tasks" do
-      project = create_project
-      task = create_task(project)
+      task = create_task(@project)
 
-      get :index, :project_id => project.id
+      get :index, :project_id => @project.id
       expect(response).to render_template(:index)
     end
   end
 
   describe 'GET #new' do
     it "assigns a new Task object" do
-      project = create_project
-      get :new, project_id: project.id
+      get :new, project_id: @project.id
       expect(assigns(:task)).to be_a_new(Task)
     end
   end
 
   describe 'POST #create' do
-    it
+    it 'creates a new task when valid parameters are passed' do
+      expect {
+        post :create, project_id: @project.id, task: {description: "Simplifying complex reality", due_date: '01/01/2000'}
+      }.to change {Task.all.count}.by(1)
+    end
+    it 'does not create a new task when invalid parameters are passed' do
+    end
+  end
 
 
 

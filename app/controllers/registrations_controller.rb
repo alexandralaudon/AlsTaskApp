5796442f493ec6ee1955118e62dd993362dc308e
Alex <alexandralaudon@gmail.com>
2015-03-23 17:37:27 -0600

    Users should be redirected back to the pages they were trying to access after logging in - syntax errors fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,8 +7,9 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = 'You have successfully signed up'
       session[:user_id] = @user.id
-      redirect_to(session[:return_to] || new_project_path), notice: 'You have successfully signed up'
+      redirect_to(session[:return_to] || new_project_path)
     else
       render :new
     end

97f30db058812aca5f3282860d7b08f18a8b0ce5
Alex <alexandralaudon@gmail.com>
2015-03-23 20:44:37 -0600

    Added Comment_spec.rb for model specs

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Comment do
+  before :each do
+    @comment1 = Comment.create(message: nil)
+    @comment2 = Comment.create(message: "Let me help you out over here")
+    @comment3 = Comment.create(message: "aaaaaaaaaaaaa")
+  end
+
+  describe '#comments' do
+    it 'returns all associated comments' do
+      # expect(Comment.all).to eq(Array(comment))
+    end
+  end
+
+  describe 'validations' do
+    it 'requires that comments have messages' do
+      expect(@comment1.errors.messages.count).to eq(1)
+      expect(@comment2.errors.present?).to eql(false)
+    end
+  end
+
+end

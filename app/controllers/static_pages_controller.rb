747580b2b0c2c820674a9c1986f8df559abba56a
Alex <alexandralaudon@gmail.com>
2015-03-23 18:40:57 -0600

    Added quotes table for StaticPages#Index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,7 @@
 class StaticPagesController < ApplicationController
+
   def index
-    @quotes =
-      [
-        { author: "-Cayla Hayes", text: "gCamp has changed my life! It's the best tool I've ever used."
-        },
-        {author: "-Leta Jaskolski", text: "Before gCamp I was a disorderly slob. Now I'm more organized than I've ever been"
-        },
-        {author: "-Lavern Upton", text: "Don't hesitate - sign up right now! You'll never be the same."
-        }
-      ]
+    @quotes = Quote.all
   end
 
   def faq
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323204516) do
+ActiveRecord::Schema.define(version: 20150324002333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20150323204516) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.string   "author"
+    t.text     "quote_text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

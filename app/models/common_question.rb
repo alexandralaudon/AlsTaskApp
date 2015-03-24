f5da2e5752fa2fa174bbc67a62f9f9893018a6ea
Alex <alexandralaudon@gmail.com>
2015-03-23 18:51:55 -0600

    Refactored CommonQuestion model

--- app/models/common_question.rb
+++ app/models/common_question.rb
@@ -1,20 +1,12 @@
 class CommonQuestion
+  attr_reader :question, :answer, :slug
 
   def initialize(question, answer)
     @question = question
     @answer = answer
     @slug = @question.downcase.gsub(" ","-")
   end
-  def question
-    @question
-  end
-  def answer
-    @answer
-  end
-  def slug
-    @slug
-  end
-
+  
   def self.create_array()
     [CommonQuestion.new("What is gCamp?", "gCamp is an awesome tool that is going to change your life. gCamp is your one stop shop to organize all your tasks. You'll also be able to track comments that you and others make. gCamp may eventually replace all need for paper and pens in the entire world. Well, maybe not, but it's going to be pretty cool."),
     CommonQuestion.new("How do I join gCamp?","As soon as it's ready for the public, you'll see a signup link in the upper right. Once that's there, just click it and fill in the form!"),

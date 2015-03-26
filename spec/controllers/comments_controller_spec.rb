fc4477848048e2da0de24404adc87b5c024f025a
Alex <alexandralaudon@gmail.com>
2015-03-26 17:45:56 -0600

    Commented out permissions spec for comments controller spec, no way to test it

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -36,4 +36,28 @@ describe CommentsController do
     end
 
   end
+
+  # describe 'Permissions' do
+  #   describe '#require_login' do
+  #     it 'redirects a nonlogged-in user to sign-in-path' do
+  #       user = create_user(email: 'tttt@ssss.com')
+  #       project = create_project
+  #       task = create_task(project)
+  #       expect {
+  #         post :create, comment: {message: ';kajshdvaid', user_id: user.id, task_id: task.id}
+  #       }.to_not change{Comment.count}
+  #
+  #       expect(response.status).to eq(404)
+  #     end
+  #     it 'allows logged-in users to create a comment' do
+  #       user = create_user(email: 'tttt@ssss.com')
+  #       session[:user_id] = user.id
+  #       project = create_project
+  #       task = create_task(project)
+  #       expect {
+  #         post :create, comment: {message: ';kajshdvaid', user_id: user.id, task_id: task.id}
+  #       }.to change{Comment.count}
+  #     end
+  #   end
+  # end
 end

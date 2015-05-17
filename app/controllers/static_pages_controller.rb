20e971e588df7f5dba46053ee5b8830935166e1b
Alex <alexandralaudon@gmail.com>
2015-05-17 07:17:49 -0600

    Updated navbar association

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   def index
     @quotes =
       [
-        { author: "-Cayla Hayes", text: "gCamp has changed my life! It's the best tool I've ever used."
+        { author: "-Cayla Hayes", text: "Al's Task App has changed my life! It's the best tool I've ever used."
         },
-        {author: "-Leta Jaskolski", text: "Before gCamp I was a disorderly slob. Now I'm more organized than I've ever been"
+        {author: "-Leta Jaskolski", text: "Before Al's Task App, I was a disorderly slob. Now I'm more organized than I've ever been!"
         },
         {author: "-Lavern Upton", text: "Don't hesitate - sign up right now! You'll never be the same."
         }
